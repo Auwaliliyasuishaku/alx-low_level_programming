4a45a6ec93cb9207a4aac97fc49c286c7e2aab72
Auwaliliyasuishaku <auwaliliyasuishaku@gmail.com>
2022-04-10 18:16:22 -0700

    88

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,23 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 90; d++)
+	for (d = 1; d < 90; d++)
 	{
-		for ()
+     		int count = 0;
+	  	for (int i = 0; i < d; i++)
+		{
+			int a = i / 10;
+			int b = i % 10;
+			int e = d / 10;
+                        int f = d % 10;
+			if ((a == f && b == e) || e == f)
+			{
+				++count;
+			}
+		}
+		if (count > 0)
+			continue;
+		else
 		{
 			putchar((d / 10) + '0');
 			putchar((d % 10) + '0');
